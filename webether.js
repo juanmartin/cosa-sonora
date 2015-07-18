e3f33a38a09cf5907e88ebbb9a13cc4e99b2a5a1
Juan Martin <juansm91@gmail.com>
2015-07-18 15:31:35 -0300

    |

--- webether.js
+++ webether.js
@@ -9,15 +9,24 @@
     webether.play = function () {
         var mySynth = flock.synth({
             synthDef: {
-                ugen: "flock.ugen.sin",
-                freq: {
-                    ugen: "flock.ugen.lfNoise",
-                    freq: 10,
-                    mul: 380,
-                    add: 60
-                },
-                mul: 0.1
-            }
+				ugen: "flock.ugen.square",
+				freq: {
+					ugen: "flock.ugen.mouse.cursor",
+					mul: 1120
+					add: 60
+					options: {
+						axis: "x",
+						target: ".pad"
+					}
+				},
+				mul: {
+					ugen: "flock.ugen.mouse.cursor",
+					options: {
+						axis: "y",
+						target: ".pad"
+					}
+				}
+			}
         });
         enviro.start();
     };
