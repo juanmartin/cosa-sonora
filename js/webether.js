5ac1dcd8e76a5ba9e2a3d82f9a8b5e587b37b628
Juan Martin <juansm91@gmail.com>
2015-07-18 21:17:44 -0300

    a

---
+++ js/webether.js
@@ -0,0 +1,25 @@
+(function () {
+
+    "use strict";
+
+    fluid.registerNamespace("webether");
+
+    var enviro = flock.init();
+
+    webether.play = function () {
+        var mySynth = flock.synth({
+            synthDef: {
+                ugen: "flock.ugen.sin",
+                freq: {
+                    ugen: "flock.ugen.sin",
+                    freq: 10,
+                    mul: 380,
+                    add: 60,
+                },
+                mul: 0.1
+            }
+        });
+        enviro.start();
+    };
+
+}());
